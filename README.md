a7e6ed8623f7c0cf195f23c7328349cbbf10574d
Yulin Zhang <yuliz12@uci.edu>
2020-03-26 20:00:42 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Project description:
+
+1. This web application helps you make your life more organized. You can add new things you want to do by simply entering it and click the plus button. When you finish doing a thing in the list, you can check the corresponding box and it will be automatically deleted from the list.
---
+++ app.js
@@ -0,0 +1,129 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const _ = require("lodash");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+// Connect with MongoDB using Mongoose
+mongoose.connect("mongodb+srv://admin-yulin:zyllxh08221024%23GZ@cluster0-yanef.mongodb.net/todolistDB", {useNewUrlParser: true});
+
+// Create a schema of a collection
+const itemsSchema = {
+  name: String
+};
+
+// Create a new model using the schema
+const Item = mongoose.model("Item", itemsSchema);
+
+// Create three default documents using the model
+const item1 = new Item({
+  name: "Enter new things to do or check the box to delete"
+});
+
+const defaultItems = [item1];
+
+const listSchema = {
+  name: String,
+  items: [itemsSchema]
+};
+
+const List = mongoose.model("List", listSchema);
+
+app.get("/", function(req, res) {
+  Item.find({}, function(err, foundItems) {
+    if(err) {
+      //console.log(err);
+    } else {
+        res.render("list", {listTitle: "Today", newListItems: foundItems});
+    }
+  });
+});
+
+app.get("/:customListName", function(req,res){
+  const customListName = _.capitalize(req.params.customListName);
+
+  List.findOne({name: customListName}, function(err, foundList) {
+    if(err) {
+      console.log(err);
+    } else {
+      if(!foundList) {
+        // Create a new list
+        const list = new List({
+          name: customListName,
+          items: defaultItems
+        });
+        list.save();
+        res.redirect("/" + customListName);
+      } else {
+        res.render("list", {listTitle: customListName, newListItems: foundList.items});
+      }
+    }
+  });
+});
+
+app.post("/", function(req, res){
+  const itemName = req.body.newItem;
+  const listName = req.body.list;
+
+  const item = new Item({
+    name: itemName
+  });
+
+  if(listName === "Today") {
+    //console.log("Successfully inserted the item into the database!");
+    if(item.name.length !== 0) {
+      item.save();
+    }
+    res.redirect("/");
+  } else {
+    List.findOne({name: listName}, function(err, foundList) {
+      if(item.name.length !== 0) {
+        foundList.items.push(item);
+        foundList.save();
+      }
+    });
+    res.redirect("/" + listName);
+  }
+});
+
+app.post("/delete", function(req, res) {
+  const checkedItemID = req.body.checkbox;
+  const listName = req.body.listName;
+
+  if(listName === "Today") {
+    Item.findByIdAndRemove(checkedItemID, function(err) {
+      if(err) {
+        //console.log(err);
+      } else {
+        //console.log("Successfully deleted the item");
+        res.redirect("/");
+      }
+    });
+  } else {
+    // Delete a document from a document array
+    List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: checkedItemID}}}, function(err, foundList) {
+      if(err) {
+        console.log(err);
+      } else {
+        //console.log("Successfully deleted the item");
+      }
+      res.redirect("/" + listName);
+    });
+  }
+});
+
+app.get("/about", function(req, res){
+  res.render("about");
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
